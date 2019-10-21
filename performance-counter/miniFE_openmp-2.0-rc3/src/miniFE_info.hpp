da1d324db4ad9d1b8fae56561ed4049dbe5151a2
Rijish Ganguly <rijish96@gmail.com>
2019-10-20 22:08:33 -0400

    Done with problem 4

---
+++ performance-counter/miniFE_openmp-2.0-rc3/src/miniFE_info.hpp
@@ -0,0 +1,13 @@
+#ifndef miniFE_info_hpp
+#define miniFE_info_hpp
+
+#define MINIFE_HOSTNAME "kraken"
+#define MINIFE_KERNEL_NAME "'Linux'"
+#define MINIFE_KERNEL_RELEASE "'4.4.0-165-generic'"
+#define MINIFE_PROCESSOR "'x86_64'"
+
+#define MINIFE_CXX "'/usr/bin/g++'"
+#define MINIFE_CXX_VERSION "'g++ (Ubuntu 5.4.0-6ubuntu1~16.04.11) 5.4.0 20160609'"
+#define MINIFE_CXXFLAGS "'-O3 -pg -fno-inline'"
+
+#endif
